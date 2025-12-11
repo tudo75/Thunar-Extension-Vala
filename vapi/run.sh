6ce62eccf4cbfe6f8d02b8a8ed540670993fe0fb
tudo75 <tudo75@hotmail.com>
2025-12-11 22:57:16 +0100

    cleaning

--- vapi/run.sh
+++ vapi/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. Generate C code
-valac -C --vapidir=. --pkg=thunarx-3 --pkg=gtk+-3.0 main.vala
+valac -C --vapidir=. --pkg=thunarx-3 --pkg=gtk+-3.0 ../src/main.vala
 
 # 2. Compile
 gcc -shared -fPIC -o libhello-plugin.so $(pkg-config --cflags --libs gtk+-3.0 thunarx-3) main.c
